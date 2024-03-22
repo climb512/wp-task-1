b0f72ef444cdc936c504bf94b578dd1b3f0ec4f5
Ted <climb512@gmail.com>
2024-03-22 16:59:17 +0700

    Basic index.php

--- app/public/wp-content/themes/customtheme/index.php
+++ app/public/wp-content/themes/customtheme/index.php
@@ -1,2 +1,46 @@
 <?php
-// Silence is golden.
+
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package YourThemeName
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <div class="container">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post();
+        ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php
+                        the_content();
+                        ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-<?php the_ID(); ?> -->
+
+            <?php endwhile;
+        else :
+            ?>
+            <p><?php esc_html_e('No posts found.', 'your-theme-name'); ?></p>
+        <?php endif; ?>
+    </div><!-- .container -->
+</main><!-- #primary -->
+
+<?php
+get_footer();
